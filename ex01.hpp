a80aa44642be5366e15342f4a50401652ae22e3f
system205 <88456547+system205@users.noreply.github.com>
2022-03-07 15:42:23 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -3,10 +3,10 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template <typename T>
-void multiplies(T& sum, T x, int n)
+void multiples(T& sum, T x, int n)
 {
   if(n < 1)
     return;
   sum += x*n;
-  multiplies (sum, x, n-1);
+  multiples (sum, x, n-1);
 }
