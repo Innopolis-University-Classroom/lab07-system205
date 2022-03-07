524282955633f4f2531a3617495384eedb13b18d
system205 <88456547+system205@users.noreply.github.com>
2022-03-07 15:40:10 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -3,7 +3,7 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 template <typename T>
-void multiples(T& sum, T x, int n)
+void multiplies(T& sum, T x, int n)
 {
   if(n < 1)
     return;
