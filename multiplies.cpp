808f958d0cd053e9fde96322aa6993aecddef4e3
system205 <88456547+system205@users.noreply.github.com>
2022-03-07 15:30:00 +0300

    Add files via upload

--- multiplies.cpp
+++ multiplies.cpp
@@ -9,6 +9,21 @@ void multiplies(T& sum, T x, int n)
   sum += x*n;
   multiplies (sum, x, n-1);
 }
+
+template <typename T>
+class Calculator
+{
+ private:
+T x, y;
+ public:
+Calculator(T x, T y) : x(x), y(y) {}
+T add(){return x + y;};
+T subtract(){return x - y;};
+T multiply(){return x * y;};
+  T divide(){return x / y;};
+
+};
+
 int main()
 {
 
