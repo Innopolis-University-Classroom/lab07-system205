1d75d1a16bd0997957c3a90f5bb8fb024ba141f2
system205 <88456547+system205@users.noreply.github.com>
2022-03-07 15:39:00 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -2,10 +2,11 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-void multiples (int& sum, int x, int n)
+template <typename T>
+void multiples(T& sum, T x, int n)
 {
- if(n < 1)
+  if(n < 1)
     return;
   sum += x*n;
-  multiples (sum, x, n-1);
+  multiplies (sum, x, n-1);
 }
